8f43d898f88cdccf5e1c806d8932cedfe11310e0
jammingend <jaimeyregeling@gmail.com>
2026-08-04 02:45:23 +0200

    refactor: wire InscriptionDataPatch, InscriptionDiagnostics, CombatDamagePatch

--- GRModApi/GRModApi.cs
+++ GRModApi/GRModApi.cs
@@ -4,7 +4,6 @@ using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using GRModApi.Modules.Inscriptions;
 using GRModApi.Modules.Inscriptions.Patches;
-using GRModApi.Test;
 using HarmonyLib;
 
 namespace GRModApi;
@@ -21,13 +20,15 @@ public class GRModApi : BasePlugin
 
         var chance = Config.Bind("Injection", "Chance", 0.25f,
             new ConfigDescription("Per-slot chance that a rolled vanilla inscription is replaced with a custom one", new AcceptableValueRange<float>(0f, 1f)));
+        var debugEnabled = Config.Bind("Debug", "Enabled", false,
+            new ConfigDescription("Enable verbose inscription diagnostic logging")).Value;
 
         InscriptionRegistry.Instance.Initialize(Log);
-        WeaponStatsPatch.Apply();
-        CombatEventsPatch.Apply();
 
         var harmony = new Harmony("JammingEnd.gr.grmodapi");
-        Test_StartingChestHook.Apply(harmony);
+        InscriptionDataPatch.Apply(harmony, Log);
+        InscriptionDiagnostics.Apply(harmony, debugEnabled);
+        CombatDamagePatch.Apply(harmony, debugEnabled, Log);
         InscriptionInjector.Apply(harmony, chance, Log);
     }
 }
